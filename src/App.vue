31a0f53dee2ade746e6730084ae39b24c8a9dd51
Chris Pryor <chrispryor@live.com>
2019-01-02 14:16:12 -0700

    import firebase to main

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <v-app>
-
+  <v-app class="grey lighten-4">
+    <Navbar />
     <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
-
   </v-app>
 </template>
 
 <script>
+import Navbar from './components/layout/Navbar'
 
 export default {
   name: 'App',
   components: {
-
+    Navbar
   },
   data () {
     return {
-      //
+
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app = null;
+
+// wait for firebase auth to init before creating the app
+firebase.auth().onAuthStateChanged(() => {
+
+  // init app if not already created
+  if(!app){
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+
+});
