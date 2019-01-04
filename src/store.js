d03a4ac63e2b2c6638840a4900d0cc1af0db6dfa
Chris Pryor <chrispryor@live.com>
2019-01-04 16:38:42 -0700

    add workout actions and mutations

--- src/store.js
+++ src/store.js
@@ -1,24 +1,52 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase';
+import db from '@/firebase/fb';
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    workouts: [
-      // {title: 'Legs', status: 'ongoing', content: 'doing four exercises', data: '2019-01-03'},
-      // {title: 'Shoulders', status: 'completed', content: 'This is a leg workout', data: '2019-01-02'},
-      // {title: 'Back', status: 'overdue', content: 'This is a back workout', data: '2019-01-01'}
-    ]
+    user: null,
+    uid: null,
+    workouts: []
   },
   getters: {
-   
+   getUser: state => {
+     return state.user;
+   },
+   getUserId: state => {
+    return state.uid;
+  }
   },
   mutations: {
-
+    setUser: (state, payload) => {
+      state.user = firebase.auth().currentUser;
+    },
+    setUserId: state => {
+      state.uid = firebase.auth().currentUser.uid;
+    },
+    updateWorkoutList: (state, payload) => {
+      state.workouts = payload;
+    }
   },
   actions: {
-
+    setUserAction: context => {
+      context.commit('setUser');
+    },
+    setUserIdAction: context => {
+      context.commit('setUserId');
+    },
+    
+    setWorkouts: context => {
+      db.collection('workouts').where('user_id', '==', firebase.auth().currentUser.uid).onSnapshot(res => {
+        const changes = res.docChanges();
+        context.commit('updateWorkoutList', changes.filter(change => change.type == 'added').map(change => ({
+          ...change.doc.data(),
+          id: change.doc.id
+        })));
+      })
+    }
   }
 })
 
